87a929068134d2285298474bee8750db3ed38bcb
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2022-06-29 20:41:25 +0900

    part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../components/layout";
 
@@ -5,6 +6,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Top">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/clifford.jpg"
+      />
     </Layout>
   );
 };
